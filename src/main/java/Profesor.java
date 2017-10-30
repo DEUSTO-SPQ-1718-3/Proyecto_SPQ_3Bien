3be5de5303d23d1139b77241873a1e964945d1f2
Aran <Aran>
2017-10-30 15:53:27 +0100

    nueva clase profesor

---
+++ src/main/java/Profesor.java
@@ -0,0 +1,101 @@
+import java.io.Serializable;
+
+public class Profesor implements Serializable{
+	
+	
+	//private static final long serialVersionUID = -5805334843360532846L;
+	private static final long serialVersionUID = 1L;
+	
+	private String nombre;
+	private String apellido;
+	private String telefono;
+	private String email;
+	private String direccion;
+	private String estudios;
+	
+	public Profesor(){
+		
+		nombre="";
+		apellido="";
+		telefono="";
+		email="";
+		direccion="";
+		estudios="";		
+	}
+	
+	
+	public Profesor (String nombre, String apellido, String telefono, String email, String direccion, String estudios){
+		
+		this.nombre=nombre;
+		this.apellido=apellido;
+		this.telefono=telefono;
+		this.email=email;
+		this.direccion=direccion;
+		this.estudios=estudios;
+		
+			
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+		
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}	
+
+	public String getDireccion(){
+		return direccion;
+	}
+
+	public void setDireccion (String direccion) {
+		this.direccion = direccion;
+	}
+	public String getEstudios() {
+		return estudios;
+	}
+	public void setEstudios(String estudios) {
+		this.estudios = estudios;
+	}	
+	
+	
+public String toString(){
+		
+		StringBuffer salida = new StringBuffer();
+		
+		salida.append("\n--------- ");
+		salida.append("\nNombre : ");
+		salida.append(nombre);
+		salida.append("\nApellido : ");
+		salida.append(apellido);
+		salida.append("\nTelefono : ");
+		salida.append(telefono);
+		salida.append("\nEmail : ");
+		salida.append(email);
+		salida.append("\nDireccion : ");
+		salida.append(direccion);
+		salida.append("\nEstudios : ");
+		salida.append(estudios);
+		
+		return salida.toString();	
+		
+	}
+	
+}
