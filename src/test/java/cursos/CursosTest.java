433a0072d85e601dec92fd69123ca2db0b479b9e
Iñaki Gavela <i.gavela@opendeusto.es>
2017-10-31 09:24:46 +0100

    Añadida estructura de clases test

---
+++ src/test/java/cursos/CursosTest.java
@@ -0,0 +1,18 @@
+package cursos;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import junit.framework.JUnit4TestAdapter;
+import cursos.clsCurso;
+
+public class CursosTest {
+	
+	public static junit.framework.Test suite() {
+		 return new JUnit4TestAdapter(CursosTest.class);
+	}
+	
+	
+}
