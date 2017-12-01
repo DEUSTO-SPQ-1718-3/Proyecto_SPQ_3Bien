c61cf2d3497cc9fdf261e3fed44c427dc8e67dcf
aranrodas <aran.rodas@opendeusto.es>
2017-12-01 13:47:51 +0100

    comentarios

--- src/main/java/profesores/frmBorrarProfesor.java
+++ src/main/java/profesores/frmBorrarProfesor.java
@@ -17,17 +17,29 @@ import java.awt.Font;
 import javax.swing.JButton;
 import javax.swing.JTextField;
 
+import org.apache.log4j.Logger;
+
+import estudiantes.frmBorrarEstudiante;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-
+/**en esta clase se recogen todos los metodos, atributos y recursos necesarios
+ * para poder borrar un profesor ya existente en la BD de la Academia
+ * 
+ * @author Grupo 3 DBS SS: Procesos software y de calidad 17-18
+ *
+ */
 //en esta clase se recogen todos los metodos, atributos y recursos necesarios para poder borrar un profesor ya existente en la BD de la Academia
 
 public class frmBorrarProfesor extends JFrame implements ActionListener{
 
+	final static Logger logger = Logger.getLogger(frmBorrarProfesor.class);
+	
 	private JPanel contentPane;
 	private JTextField textFieldDni;
 	MyDataAccess conexion = new MyDataAccess();
+	
 	
 	/**
 	 * Create the frame.
@@ -69,21 +81,25 @@ public class frmBorrarProfesor extends JFrame implements ActionListener{
 		switch(e.getActionCommand()){
 		
 		
-			case "Atras":
-				
-				this.dispose();				
+			case "Atras":	
+				this.dispose();	
+				logger.trace("This is TRACE : Se ha destruido la ventana de borrar profesor");
 				break;
+			
 				
 			case "Aceptar":
 				String dni=textFieldDni.getText();
 				borrarProfesor(dni);
-				
-				
+					
 				break;
 		}
 		
 	}
 	
+	/**
+	 * Se borran de la BD los datos correspondientes al dni del profesor que se pasa por parametro
+	 * @param dni
+	 */
 	
 	void borrarProfesor(String dni) {
 		// TODO Auto-generated method stub
@@ -91,6 +107,8 @@ public class frmBorrarProfesor extends JFrame implements ActionListener{
 		String borrar= "Delete from profesores where dni='"+dni+"' ";
 		//enviar la sentencia para borrar el profesor indicado mediante el nombre de la BD
 		conexion.setQuery(borrar);
+		
+		logger.info("This is INFO : Se ha eliminado un profesor de la base de datos");
 		
 		this.dispose();
 		JOptionPane.showMessageDialog(this,"El/la profesor/a con dni "+dni + " ha sido borrado/a correctamente");
